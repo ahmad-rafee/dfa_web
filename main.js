587badd8e8fc1b33b8392aa86eefea8def971e8a
unknown <al7oot482014@gmail.com>
2018-12-18 12:01:56 +0200

    Inititalizing...

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const http = require('http');
 const app = express();
+
 const server = http.createServer(app);
 app.use("/",express.static("."));
 server.listen(5000 || process.env.PORT,()=>{
